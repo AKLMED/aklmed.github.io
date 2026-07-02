6d7d269f44f1efafe2ae7b4391e956dbe5cf53c2
AKLMED <3131049426@qq.com>
2026-07-02 14:21:10 +0800

    小修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,9 +50,10 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/AKLMED/aklmed.github.io/tree/main',
+            'https://github.com/AKLMED/aklmed.github.io/tree/gh-pages',
           showLastUpdateTime: true, 
           showLastUpdateAuthor: true,
+
         },
         blog: false,/*{
           showReadingTime: true,
